6b2101e43eb8c6aadcf948bd10fad1069d69fb68
Danil <klimenko.apple01@icloud.com>
2022-02-16 17:41:45 +0200

    Digits buttons and methods are done

--- SimpleCalculator/ViewController.swift
+++ SimpleCalculator/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    var numberFromScreen: Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        resultLabel.text = ""
     }
 
 
+    @IBAction func digitPressed(_ sender: UIButton) {
+        
+        resultLabel.text = resultLabel.text! + String(sender.tag)
+        numberFromScreen = Double(resultLabel.text!)!
+    }
+    
+    @IBAction func controlPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+    
 }
 
